79ada5a0a8bc0420f463f2c2dbe4395e4530aabe
VirgiRobledo <virgirobledo.88@gmail.com>
2024-07-07 12:53:01 -0300

    Se crea toda la funcionalidad en delete, se modifica desde index.html y en app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
-from flask import render_template,request,redirect
+from flask import render_template,request,redirect, send_from_directory
 from flaskext.mysql import MySQL
+import datetime
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -29,6 +31,10 @@ def index():
     #conn.close()  
     return render_template('index.html')
 
+# Configurar la ruta para servir archivos estáticos
+@app.route('/src/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory('src/uploads', filename)
 
 @app.route('/create')
 def create():
@@ -39,9 +45,23 @@ def store():
     _nombre=request.form['txtNombre']
     _correo=request.form['txtCorreo']
     _foto=request.files['txtFoto']
+    
+   # Suponiendo que _foto es una instancia de werkzeug.datastructures.FileStorage
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Crear el directorio 'uploads' si no existe
+    upload_dir = 'src/uploads'
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+
+    if _foto.filename != '':
+        nuevoNombreFoto= f"{timestamp}_{_foto.filename}"
+         # Guardar la foto en el sistema de archivos
+        _foto.save(os.path.join(upload_dir, nuevoNombreFoto))
+
     sql="INSERT INTO empleados (nombre, correo, foto) VALUES (%s,%s,%s);"
 
-    datos=[_nombre, _correo, _foto.filename]
+    datos=(_nombre, _correo, nuevoNombreFoto)
     conn = mysql.connect()  # Llama a la función connect()
     cursor = conn.cursor()  # Obtiene el cursor de la conexión
 
@@ -49,6 +69,17 @@ def store():
     conn.commit()
 
     return redirect('/')  
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    sql = "DELETE FROM empleados WHERE id=%s;"
+   
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute(sql, (id,))
+    conn.commit()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
